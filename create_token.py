6a4e537ce42926e220f0211371fac6f8c9478c79
Marko <marko.ivankovic650@gmail.com>
2020-07-30 03:42:23 +0200

    Made functions for retrieving realm and AH info.

--- create_token.py
+++ create_token.py
@@ -1,13 +1,47 @@
 import requests
+from requests.auth import HTTPBasicAuth
 
-client_id = '4765849c9d3a4ae7a1132515105cba14'
-client_secret = '0rR3xvKEtE56pjSMdT16i3DO6pEPf0eM'
-data = {'grant_type': 'client_credentials'}
 
-# Response is a dictionary with k, v pairs:
+def retrieve_token(client_id, client_secret, region='eu'):
+    url = f'https://{region}.battle.net/oauth/token'
+    body = {'grant_type': 'client_credentials'}
+    auth = HTTPBasicAuth(client_id, client_secret)
+
+    response = requests.post(url, data=body, auth=auth)
+    return response.json()
+
+
+def make_request(url):
+    base_url = 'https://eu.api.blizzard.com/data/wow/connected-realm/'
+    response = requests.get(base_url + url)
+    return response.json()
+
+
+CLIENT_ID = '4765849c9d3a4ae7a1132515105cba14'
+CLIENT_SECRET = 'WZrsyckvjFLXVyNxEnGKCuS3is8I5Spw'
+
+# token_info is a dictionary with k, v pairs:
 # "access_token": "USVb1nGO9kwQlhNRRnI4iWVy2UV5j7M6h7",
 # "token_type": "bearer",
 # "expires_in": 86399,
 # "scope": "example.scope"
 
-response = requests.post('https://eu.battle.net/oauth/token', data=data, auth=(client_id, client_secret)).json()
+token_info = retrieve_token(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET)
+
+access_token = token_info['access_token']
+
+# Retrieve ID's of available EU servers
+connected_realm_IDs = make_request(
+    f'index?namespace=dynamic-eu&locale=en_GB&access_token={access_token}')
+
+server_id = 1302  # Archimonde EU server ID
+
+# Retrieve info for specific server ID
+connected_realm_info = make_request(
+    f'{server_id}?namespace=dynamic-eu&locale=en_GB&access_token={access_token}')
+
+# Archimonde server AH data
+ah_data = make_request(
+    f'{server_id}/auctions?namespace=dynamic-eu&locale=en_GB&access_token={access_token}')
+print('Hello')
