068df0ea11f919d8a11645a1fc0ef1216fbd6ed7
Marko <marko.ivankovic650@gmail.com>
2020-07-31 04:30:59 +0200

    connect to newswire, pulling data.

--- newswire_pull_data.py
+++ newswire_pull_data.py
@@ -11,14 +11,23 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+# Get table names
+mycursor.execute("SHOW TABLES")
+table_names = mycursor.fetchall()
+
 # Get column names
-mycursor.execute("SHOW COLUMNS FROM tblWowToken")
+mycursor.execute("SHOW COLUMNS FROM tblItemHistoryMonthly")
 column_names = mycursor.fetchall()
 
-# mycursor.execute("SHOW TABLES")
-mycursor.execute("SELECT * FROM tblWowToken WHERE YEAR(`when`)>2019 AND region='EU'")
-myresult = mycursor.fetchall()
+# mycursor.execute("SELECT * FROM tblItemHistoryMonthly WHERE item = 65891 ORDER BY month ASC LIMIT 100")
+# myresult = mycursor.fetchall()
 
-c = csv.writer(open('wow-token_2020.csv', 'w'))
-for x in myresult:
-    c.writerow(x)
+mycursor.execute("SELECT DISTINCT(month) AS Months FROM tblItemHistoryMonthly ORDER BY Months DESC")
+distinct_month_vals = mycursor.fetchall()
+print('Hello')
+# mycursor.execute("SELECT * FROM tblWowToken WHERE YEAR(`when`)>2019 AND region='EU'")
+# myresult = mycursor.fetchall()
+
+# c = csv.writer(open('wow-token_2020.csv', 'w'))
+# for x in myresult:
+#     c.writerow(x)
