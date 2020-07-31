45cc312d2cf6a806d15391082d891c91f1b45cf9
Marko <marko.ivankovic650@gmail.com>
2020-07-31 14:15:03 +0200

    pulled monelite ore prices from newswire

--- newswire_pull_data.py
+++ newswire_pull_data.py
@@ -16,18 +16,22 @@ mycursor.execute("SHOW TABLES")
 table_names = mycursor.fetchall()
 
 # Get column names
-mycursor.execute("SHOW COLUMNS FROM tblItemHistoryMonthly")
+mycursor.execute("SHOW COLUMNS FROM tblItemHistoryDaily")
 column_names = mycursor.fetchall()
 
 # mycursor.execute("SELECT * FROM tblItemHistoryMonthly WHERE item = 65891 ORDER BY month ASC LIMIT 100")
 # myresult = mycursor.fetchall()
+query = "SELECT DBItm.name_enus, IHD.* FROM tblItemHistoryDaily IHD JOIN tblDBCItem DBItm ON IHD.item = DBItm.id JOIN tblRealm RLM ON RLM.house = IHD.house WHERE RLM.region = 'EU' AND RLM.slug = 'doomhammer' AND DBItm.name_enus IN ('Monelite Ore') ORDER BY `when` DESC"
+mycursor.execute(query)
+monelite_prices = mycursor.fetchall()
 
-mycursor.execute("SELECT DISTINCT(month) AS Months FROM tblItemHistoryMonthly ORDER BY Months DESC")
-distinct_month_vals = mycursor.fetchall()
-print('Hello')
+header = ['item_name', 'item_id', 'house', 'date', 'pricemin', 'priceavg', 'pricemax', 'pricestart', 'priceend',
+          'quantitymin', 'quantityavg', 'quantitymax']
 # mycursor.execute("SELECT * FROM tblWowToken WHERE YEAR(`when`)>2019 AND region='EU'")
 # myresult = mycursor.fetchall()
 
-# c = csv.writer(open('wow-token_2020.csv', 'w'))
-# for x in myresult:
-#     c.writerow(x)
+with open('monelite-ore-prices.csv', 'w+', newline='') as c:
+    cw = csv.writer(c)
+    cw.writerow(header)
+    for x in monelite_prices:
+        cw.writerow(x)
