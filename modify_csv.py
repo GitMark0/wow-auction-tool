341ad1a55181ad5c4240be530acc67f17fe69253
marko <marko.ivankovic650@gmail.com>
2020-08-02 00:39:39 +0200

    added script to modify csv files from database-2

--- modify_csv.py
+++ modify_csv.py
@@ -1,17 +1,5 @@
 import pandas as pd
 import math
-<<<<<<< HEAD
-
-vos_tbl = pd.read_csv('VoS_2019-2020.csv')
-
-rows_list = []
-for index, row in vos_tbl.iterrows():
-    for i in range(1, 32):
-        suffix = f'0{i}' if i < 10 else str(i)
-        row_date = str(int(row['Year'])) + '-' + str(int(row['monthVal'])) + '-' + str(i)
-        row_quantity = 0 if math.isnan(row['qty' + suffix]) else int(row['qty' + suffix])
-        row_price = 0 if math.isnan(row['mktslvr' + suffix]) else int(row['mktslvr' + suffix])
-=======
 import calendar
 
 c = calendar.Calendar()
@@ -29,12 +17,7 @@ for index, row in vos_tbl.iterrows():
         row_date = f'{year}-{month}-{i}'
         row_quantity = 0 if math.isnan(row['qty' + suffix]) else int(row['qty' + suffix])
         row_price = 0 if math.isnan(row['mktslvr' + suffix]) else round(int(row['mktslvr' + suffix])/100, 2)
->>>>>>> develop
         rows_list.append({'date': row_date, 'quantity': row_quantity, 'price': row_price})
 
 vos_modified = pd.DataFrame(rows_list)
 vos_modified.to_csv('VoS-m_2019-2020.csv')
-<<<<<<< HEAD
-print('Hello')
-=======
->>>>>>> develop
