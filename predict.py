78ba8e50dcf4c5e50bab43c04aa59835a2de8e9f
marko <marko.ivankovic650@gmail.com>
2020-08-04 17:45:33 +0200

    finished prediction class

--- predict.py
+++ predict.py
@@ -2,18 +2,24 @@ from model import NN
 import torch
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 from data_preparation import encode
+
+scaler = MinMaxScaler()
 
 
 def predict(X):
     nn = NN([6, 8, 4, 1])
     nn.load_state_dict(torch.load('model_state/state'))
     nn.eval()
-    result = nn.forward(X)
-    return result.detach().numpy()
+    result = nn.forward(X).detach().numpy().reshape(-1, 1)
+    result_transformed = scaler.inverse_transform(result)
+    return result_transformed
 
 
 if __name__ == '__main__':
+    data = pd.read_csv('datasets/anchor-weed.csv')
+    scaler.fit_transform(data[['price']])
 
     print('quantity, price, dow, month')
     vals = input().split()
@@ -30,4 +36,4 @@ if __name__ == '__main__':
     X = vals_df.values
     X = torch.from_numpy(X).float()
     val = predict(X)
-    print(val)
+    print(round(val[0][0]/100, 2))
